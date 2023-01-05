e52afc2e644093f286704be9d891c3bfb549f784
Mike Weaver <michael@triplewhale.com>
2023-01-05 14:30:46 -0500

    Update site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://tw-dev-docs.netlify.app/',
   integrations: [sitemap(), vue()],
   markdown: {
     remarkPlugins: [remarkReadingTime, 'remark-code-titles'],
